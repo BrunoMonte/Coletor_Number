4a8aeb0e13d0dffe049cce01e041d7e18d1b8d50
Bruno Monte <brunoaguiarprofissional@gmail.com>
2020-07-11 08:21:50 -0300

    comentários js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
-let valores = []
+let valores = []        //vetor, com esse vetor analisar os dados , array.
 
 
-function isNumero2(n){
-    return (Number(n) >= 1 && Number(n) <= 100)
+function numeroAdc(n){                          //recebe um numero no paramentro
+    return (Number(n) >= 1 && Number(n) <= 100)     //só aceita se for numero entre 1 e 100
 
 }
 
-function inLista(n,l){
-    if (l.indexOf(Number(n)) != -1){
+function numeroLista(n,l){                      //recebe um numero, e uma lista representada pelo 1
+    if (l.indexOf(Number(n)) != -1){            // se na lista o number de N for diferente de -1..
         return true
     } else {
         return false
@@ -18,43 +18,44 @@ function inLista(n,l){
 }
 
 function adicionar(){
-    if(isNumero2(num.value) && !inLista(num.value, valores)){
-        valores.push(Number(num.value))
-        let item = document.createElement('option')
-        item.text = 'Valor '+num.value+' adicionado'
-        lista.appendChild(item)
+    if(numeroAdc(num.value) && !numeroLista(num.value, valores)){           //adicionar se for um numero, e se numero nao estiver na lista e valores para saber se esta na lista.
+        valores.push(Number(num.value))                                 //push adicionar um elemento no vetor, adicionar number dentro de value
+        let item = document.createElement('option')                     //criando elemento dentro do item
+        item.text = 'Valor '+num.value+' adicionado'            //msg ser escrita no item
+        lista.appendChild(item)                 //para numero e msg ir para lista.
         res.innerHTML = ""                 //quando eu conseguir adicionar elemento, tem que limpar resultado
     
     }else {
         window.alert('Valor inválido ou já encontrado na lista !!')
     }
-    num.value = "" //para ele apagar no bloco
+    num.value = "" //para ele apagar no bloco no numero adicionado
     num.focus()    // voltar cursor apos acrescentar
 }
 
 function finalizar(){
-    if(valores.length == 0){
+    if(valores.length == 0){        // Se o vetor estiver vazio,mostrar msg
         window.alert('Adicione valores antes de finalizar!')
     }else{
-        let tot = valores.length
+        let tot = valores.length        //saber quantos numeros foi adcionado
         let maior = valores[0]
         let menor = valores [0]
         let soma = 0
         let media = 0
-        for(let pos in valores){
-            soma += valores[pos]
-            if(valores[pos] > maior)
+        for(let pos in valores){            //teste para saber o maior e menor numero adicionado
+            soma += valores[pos]                // para fazer a soma do valor atual com posterior
+            if(valores[pos] > maior)                //Deixa de ser valor anterior para ser o maior encontrado
                 maior = valores[pos]
             if (valores[pos] < menor)
                 menor = valores[pos]    
         }
-        media = soma / tot
+        media = soma / tot  //tirar a media dos valores
         res.innerHTML = ""    //para ele zerar o valor
         res.innerHTML += '<p>Ao todo, temos '+tot+' números cadastrados</p>'
         res.innerHTML +='<p>O maior valor informado foi '+maior+'.</p>'
         res.innerHTML +='<p>O menor valor informado foi '+menor+'.</p>'
         res.innerHTML +='<p>Somando todos valores são '+soma+'.</p>'
         res.innerHTML +='<p>A media dos valores digitados são '+media+'.</p>'
+        res.innerHTML +='<p>Tem '+par+' valores pares.</p>'
     }
 }
 
